2705a3f8f3cf909bfa3540bf2fd417eb3655843c
Wilson <wilsonfzheng@gmail.com>
2024-09-25 08:06:34 -0400

    set up backend get request

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,32 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
+#Create flask
 app = Flask(__name__)
 CORS(app)
 
-#Sample
-users = {
-    "123": {"username": "John", "mood": "Happy"},
-    "124": {"username": "Jane", "mood": "Sad"},
-}
+#Setup database URI to use SQLite database, create database called users.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#Create tables if they don't exist
+with app.app_context():
+    db.create_all()
+
+class User(db.Model):
+    id = db.Column(db.String(3), primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    mood = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return f'<User: {self.username}, Mood: {self.mood}>'
 
 @app.route('/get_users/<user_id>', methods=['GET'])
 def get_user(user_id):
     # Check if the user_id exists in our data
-    user = users.get(user_id)
+    user = User.query.get(user_id)
     if user:
         return jsonify({
             "user_id": user_id,
