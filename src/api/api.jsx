d4648be02a67d9954da05d10cc15942ddcaefcb9
abhay <abhay.i0am@gmail.com>
2024-09-28 23:02:24 +0530

    api

---
+++ src/api/api.jsx
@@ -0,0 +1,60 @@
+
+//will need to update the endpointss
+import react, { useState } from "react";
+function fetchapi(){
+    const [data, setdata] = useState([]);
+    const [newitem, setnewitem] = useState('');
+    const [loggedin, setlogin] = useState(false);
+    const [username, password] = useState('');
+
+    const fetchdata = async () => {
+        try {
+          const response = await fetch('/api/data'); 
+          const jsondata = await response.json();
+          setdata(jsondata.data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+    const postdata = async () => {
+        try {
+          const response = await fetch('/api/data', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ item: newitem })
+          });
+    
+          if (response.ok) {
+            console.log('item added');
+            setnewitem('');
+            fetchdata();
+          } else {
+            console.error('Error adding item');
+          }
+        } catch (error) {
+          console.error('Error posting data:', error);
+        }
+      };
+
+    const handlelogin = async () => {
+        try {
+            const response = await fetch('/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username, password})
+            });
+            const data = await response.json();
+        }
+        catch(error){
+            console.log("error logging in:", error)
+    }
+    };
+      
+}
+
+export default fetchapi;
