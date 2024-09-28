b6d0a319b1c7da4feab0c457d9790bf04d0befaf
Wilson <wilsonfzheng@gmail.com>
2024-09-28 07:32:44 -0400

    added some APIs

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask import request
 
 #Create flask
 app = Flask(__name__)
@@ -23,15 +24,52 @@ class User(db.Model):
     def __repr__(self):
         return f'<User: {self.username}, Mood: {self.mood}>'
 
+#Add a new user to the database
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    if 'id' not in data or 'username' not in data or 'mood' not in data:
+        return jsonify({"error": "Missing required fields"}), 400
+
+    new_user = User(id=data['id'], username=data['username'], mood=data['mood'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User added successfully"}), 201
+
+#Update a user in the database
+@app.route('/update_user/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        data = request.get_json()
+        user.username = data['username']
+        user.mood = data['mood']
+        db.session.commit()
+        return jsonify({"message": "User updated successfully"}), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+#Delete a user from the database
+@app.route('/delete_user/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": "User deleted successfully"}), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+#Retrieve user information from database
 @app.route('/get_users/<user_id>', methods=['GET'])
 def get_user(user_id):
     # Check if the user_id exists in our data
     user = User.query.get(user_id)
     if user:
         return jsonify({
-            "user_id": user_id,
-            "username": user["username"],
-            "mood": user["mood"]
+            "user_id": user.id,
+            "username": user.username,
+            "mood": user.mood
         })
     else:
         return jsonify({"error": "User not found"}), 404
